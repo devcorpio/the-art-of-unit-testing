279aacc2b855ca9f530b12da0df893297afd0e4c
devcorpio <adelgado.devcorpio@gmail.com>
2019-01-09 13:06:58 +0100

    chapter 02: returns error if the filename is empty

--- chapter_02-a-first-unit-test/LogAn/logAnalyzer.js
+++ chapter_02-a-first-unit-test/LogAn/logAnalyzer.js
@@ -1,9 +1,15 @@
+const ArgumentError = require('./ArgumentError');
+
 function logAnalyzer() {
     /**
      * @param {string} fileName
      * @return {boolean}
      */
     function isValidLogFileName(fileName) {
+        if (fileName === '') {
+            throw new ArgumentError('filename has to be provided');
+        }
+
         if (!fileName.toUpperCase().endsWith('.SLF')) {
             return false;
         }
