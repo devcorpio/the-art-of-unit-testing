7e50568fa812cdaaf0c8e48fb58d643979b85a52
devcorpio <adelgado.devcorpio@gmail.com>
2019-01-09 13:16:38 +0100

    chapter 02: add test that assert that the error is thrown

--- chapter_02-a-first-unit-test/LogAn/logAnalyzer.test.js
+++ chapter_02-a-first-unit-test/LogAn/logAnalyzer.test.js
@@ -1,17 +1,27 @@
 const logAnalyzer = require('./logAnalyzer');
 
+let logAnalyzerInstance;
+beforeEach(() => {
+    logAnalyzerInstance = logAnalyzer();
+});
+
 describe.each([
     ['johndoe.js', false],
     ['johndoe.slf', true],
     ['johndoe.SLF', true],
 ])('isValidLogFileName("%s"))', (fileName, expected) => {
-    let logAnalyzerInstance;
-    beforeEach(() => {
-        logAnalyzerInstance = logAnalyzer();
-    });
-
     it(`bad extension returns ${expected}`, () => {
         const result = logAnalyzerInstance.isValidLogFileName(fileName);
         expect(result).toBe(expected);
+    });
+});
+
+describe('isValidLogFileName', () => {
+    it('empty filename throws error', () => {
+        function emptyLogFileName() {
+            logAnalyzerInstance.isValidLogFileName('');
+        }
+
+        expect(emptyLogFileName).toThrow('filename has to be provided');
     });
 });
