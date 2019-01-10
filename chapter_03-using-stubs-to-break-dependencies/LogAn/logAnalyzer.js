95768cfce8e5bfe0e4155b9e8153e495b6e80678
devcorpio <adelgado.devcorpio@gmail.com>
2019-01-10 20:25:58 +0100

    chapter 03: check the validity of the file extension in a disk file, that creates an external dependency, unit tests are broken

--- chapter_03-using-stubs-to-break-dependencies/LogAn/logAnalyzer.js
+++ chapter_03-using-stubs-to-break-dependencies/LogAn/logAnalyzer.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const util = require('util');
 const ArgumentError = require('./ArgumentError');
+
+const readFilePromisied = util.promisify(fs.readFile);
 
 function logAnalyzer() {
     /**
@@ -15,21 +19,49 @@ function logAnalyzer() {
 
     /**
      * @param {string} fileName
-     * @return {boolean}
+     * @return {Promise}
      */
-    function isValidLogFileName(fileName) {
+    async function isValidLogFileName(fileName) {
         wasLastFileNameValid = false;
 
         if (fileName === '') {
             throw new ArgumentError('filename has to be provided');
         }
 
-        if (!fileName.toUpperCase().endsWith('.SLF')) {
+        const result = await isValid(fileName);
+
+        if (!result) {
             return false;
         }
 
         wasLastFileNameValid = true;
         return true;
+    }
+
+    /**
+     * @param {string} fileName
+     */
+    async function isValid(fileName) {
+        const fileNameExtensions = await readFilePromisied(
+            `${__dirname}/fileNameExtensions.config.json`,
+            'utf8'
+        ).then(fileContent => JSON.parse(fileContent).extensions);
+
+        const isValidExtension = fileNameExtensions.some(
+            function checkFileNameExtension(extension) {
+                if (
+                    fileName
+                        .toUpperCase()
+                        .endsWith(`.${extension.toUpperCase()}`)
+                ) {
+                    return true;
+                }
+
+                return false;
+            }
+        );
+
+        return isValidExtension;
     }
 
     return {
